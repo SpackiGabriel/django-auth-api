7831299aeb114c54a2b68f6a6e0e9a386fad1cf4
Gabriel Spacki <ifprgabriel@gmail.com>
2024-04-07 02:11:30 -0300

    added login routine

--- server/views.py
+++ server/views.py
@@ -4,12 +4,43 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 
 
 @api_view(['POST'])
 def login(request: Request) -> Response:
-    return Response({})
+    """
+    Logs in a user and returns a token and user information.
+
+    Args:
+        request (Request): The HTTP request object.
+
+    Returns:
+        Response: The HTTP response object containing the token and user information.
+
+    Raises:
+        Http404: If the user with the specified username is not found.
+    """
+    user = get_object_or_404(User, username=request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response(
+            {
+                'error': 'Invalid password'
+            },
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    token, _ = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+
+    return Response(
+        {
+            'token': token.key,
+            'user': serializer.data
+        }
+    )
 
 @api_view(['POST'])
 def register(request: Request) -> Response:
@@ -23,7 +54,7 @@ def register(request: Request) -> Response:
         Response: The HTTP response object containing the token and user data if successful,
         or the error message and status code if unsuccessful.
     """
-    serializer = UserSerializer(data=request.data)
+    serializer = UserSerializer(data = request.data)
     
     if serializer.is_valid():
         user = serializer.save()
@@ -31,7 +62,7 @@ def register(request: Request) -> Response:
         user.set_password(request.data['password'])
         user.save()
 
-        token = Token.objects.create(user=user)
+        token = Token.objects.create(user = user)
         
         return Response(
             {
@@ -40,8 +71,7 @@ def register(request: Request) -> Response:
             }
         )
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def verify(request: Request) -> Response:
