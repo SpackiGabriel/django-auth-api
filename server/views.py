abf1c9a36df46efbb7867d6ed1e8adf3abb515ec
Gabriel Spacki <ifprgabriel@gmail.com>
2024-04-07 02:03:41 -0300

    added register routine

--- server/views.py
+++ server/views.py
@@ -9,13 +9,40 @@ from .serializers import UserSerializer
 
 @api_view(['POST'])
 def login(request: Request) -> Response:
-    return Response
+    return Response({})
 
 @api_view(['POST'])
 def register(request: Request) -> Response:
-    return Response
+    """
+    Register a new user.
+
+    Args:
+        request (Request): The HTTP request object.
+
+    Returns:
+        Response: The HTTP response object containing the token and user data if successful,
+        or the error message and status code if unsuccessful.
+    """
+    serializer = UserSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        user = serializer.save()
+
+        user.set_password(request.data['password'])
+        user.save()
+
+        token = Token.objects.create(user=user)
+        
+        return Response(
+            {
+                'token': token.key,
+                'user': serializer.data
+            }
+        )
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
 def verify(request: Request) -> Response:
-    return Response
+    return Response({})
